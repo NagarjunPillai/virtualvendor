a717816a63ac1ee300fe43111d6f864d4e542a6c
NagarjunPillai <ad.aa906@gmail.com>
2020-04-24 21:51:04 +0530

    Trial

---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class OptionsMenu : MonoBehaviour {
+
+	[SerializeField] private Image menu;
+	// Use this for initialization
+	void start()
+	{
+		menu.enabled = false;
+	}
+
+	void main()
+	{
+		menu.enabled = false;
+	}
+
+
+}
---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UCL.COMPGV07;
+
+public class PauseMenu : MonoBehaviour {
+
+	public static bool GameIsPaused = false;
+	public GameObject pauseMenuUI;
+	//public GameObject fps;
+
+
+	// Update is called once per frame
+	public void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Q))
+			{
+			if (GameIsPaused) {
+				Resume();
+			}
+			else {
+				
+				Pause ();
+			}
+			}
+		
+	}
+
+	public void Resume(){
+				pauseMenuUI.SetActive (false);
+		//fps.SetActive (true);
+				Time.timeScale = 1f;
+				GameIsPaused = false;
+
+	}
+	public void quit()
+	{
+		Application.Quit ();
+		Program trial = new Program ();
+		//trial.Main2 ();
+
+	}
+	public void Pause(){
+		pauseMenuUI.SetActive (true);
+		//fps.SetActive (false);
+
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+
+	}
+	public void metrics()
+	{
+		Program trial = new Program ();
+		trial.Main2();
+
+	}
+
+
+}
